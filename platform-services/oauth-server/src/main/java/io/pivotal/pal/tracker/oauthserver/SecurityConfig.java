dea9c3760e58eb6f7b6f82a7899f538270355b8f
kundanprasadsah <kundan.prasadsah@cognizant.com>
2020-03-29 20:12:46 +0100

    Lab 12 - Securing a Distributed System

---
+++ platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/SecurityConfig.java
@@ -0,0 +1,29 @@
+package io.pivotal.pal.tracker.oauthserver;
+
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+public class SecurityConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("tracker-client")
+                .secret("supersecret")
+                .authorizedGrantTypes("client_credentials")
+                .scopes("openid");
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer)
+            throws Exception {
+        oauthServer
+                // NoOpPasswordEncoder is not secure but will be OK for local testing purposes.
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
+    }
+}
