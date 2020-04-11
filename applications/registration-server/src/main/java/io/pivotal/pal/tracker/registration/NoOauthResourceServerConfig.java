c2ad37e64f0119b3328bae4e8bf1d06b79c41d09
kundanprasadsah <kundan.prasadsah@cognizant.com>
2020-04-11 01:45:33 +0100

    Lab 12 - Securing a Distributed System

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/NoOauthResourceServerConfig.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/NoOauthResourceServerConfig.java
@@ -18,9 +18,11 @@ public class NoOauthResourceServerConfig extends ResourceServerConfigurerAdapter
         http.authorizeRequests().anyRequest().permitAll();
     }
 
+    /*
     @Bean
     @LoadBalanced
     public RestOperations restOperations() {
         return new RestTemplate();
     }
+     */
 }
